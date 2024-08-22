2f4c123319bc96f46692a5778deeb1636ae38a01
Seongbeom74 <seongbeom7047@gmail.com>
2024-08-22 22:09:07 +0800

    안녕하세요

---
+++ HW/Solution.java
@@ -0,0 +1,16 @@
+package HW;
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigit = new int[digits.length];
+        newDigit[0] = 1;
+        return newDigit;
+    }
+}
