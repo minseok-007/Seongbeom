f86a4d43ca831a0afe0656f26c70694f4dca097b
Minseok Chey <mc1920@ic.ac.uk>
2024-10-24 13:41:15 +0100

    Practice Problems

--- HW3/Anagram.java
+++ HW3/Anagram.java
@@ -1,6 +1,7 @@
 package HW3;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Anagram {
     public static void main(String[] args) {
@@ -13,6 +14,34 @@ public class Anagram {
         if (s.length() != t.length()) return false;
 
         HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
 
+//        HashMap<Character, Integer> mapT = new HashMap<>();
+//        for (char c : t.toCharArray()) {
+//            mapT.put(c, mapT.getOrDefault(c, 0) + 1);
+//        }
+//
+//        for (Map.Entry<Character, Integer> entry : mapS.entrySet()) {
+//            char c = entry.getKey();
+//            int freq = entry.getValue();
+//
+//            if (!mapT.containsKey(c) || mapT.get(c) != freq) {
+//                return false;
+//            }
+//        }
+//        return true;
+
+        for (char c: t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+        return map.isEmpty();
     }
 }
